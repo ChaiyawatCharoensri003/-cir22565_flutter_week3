a5042bc952ed39c82a6490c1545ea8e6f3eb5cd0
ChaiyawatCharoensri003 <airsupansa23@gmail.com>
2022-11-03 11:51:01 +0700

    first commit

--- lib/MyApp.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-void main (){
-  run App (const MyApp()):
-
-}
-class MyApp extends StatelessWidget{
-  const MyApp ({Key?key}): super (key:key);//1.const
-}
