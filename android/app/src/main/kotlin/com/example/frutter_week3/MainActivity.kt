02ba6b8d8388622997ce48f0c6710f2abb953b74
ChaiyawatCharoensri003 <airsupansa23@gmail.com>
2022-11-03 11:48:47 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/frutter_week3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.frutter_week3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/MyApp.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+void main (){
+  run App (const MyApp()):
+
+}
+class MyApp extends StatelessWidget{
+  const MyApp ({Key?key}): super (key:key);//1.const
+}
